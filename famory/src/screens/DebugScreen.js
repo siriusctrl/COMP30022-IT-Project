e7a790484ec0a50839728be4cc197e0b6cf846a4
Xinyao Niu <morryniu123@gmail.com>
2019-08-18 16:15:26 +1000

    backend controller and  debug pages

---
+++ famory/src/screens/DebugScreen.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import { Text, Image, StyleSheet, View , Alert, KeyboardAvoidingView, ImageBackground} from "react-native";
+
+import Button from "../components/Button";
+import FormTextInput from "../components/FormTextInput";
+import colors from "../config/colors";
+import strings from "../config/strings";
+import { FlatList } from "react-native-gesture-handler";
+
+export default class HomePageScreen extends Component{
+  render() {
+    return (
+      <FlatList>
+        
+      </FlatList>
+    );
+  }
+}
