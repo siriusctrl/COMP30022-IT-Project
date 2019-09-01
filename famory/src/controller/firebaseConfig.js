025d0c859fbc4cdf45792e3d6d5cc185eccf6d0a
Shouyin <7000kl@163.com>
2019-09-01 14:46:55 +1000

    auto stage

---
+++ famory/src/controller/firebaseConfig.js
@@ -0,0 +1,6 @@
+const firebaseConfig = {
+  apiKey: "<YOUR-API-KEY>",
+  authDomain: "<YOUR-AUTH-DOMAIN>",
+  databaseURL: "<YOUR-DATABASE-URL>",
+  storageBucket: "<YOUR-STORAGE-BUCKET>"
+};
---
+++ famory/src/screens/ArtGuide.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import { Text, Image, StyleSheet, View , Alert, KeyboardAvoidingView, ImageBackground} from "react-native";
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import colors from "../config/colors";
+
+import ArtCard from "../components/ArtCard";
+import { TouchableNativeFeedback, TouchableHighlight } from "react-native-gesture-handler";
+
+
+
