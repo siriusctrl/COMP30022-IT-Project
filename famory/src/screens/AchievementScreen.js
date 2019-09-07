3e11431ea797b09e96ee5acd7e96f61f4c462ecc
sahuang <xiaohaix@student.unimelb.edu.au>
2019-09-07 14:04:28 +1000

    Achievement Layout Done

--- famory/src/screens/AchievementScreen.js
+++ famory/src/screens/AchievementScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {View, StyleSheet, Text, Image} from 'react-native';
-import { Icon } from 'native-base';
+import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
+import Modal from "react-native-modal";
 import colors from "../config/colors";
 
 import BadgeCBL from "../assets/icons/Badges/badgeCommBLocked";
@@ -24,8 +24,23 @@ import BadgeHGN from "../assets/icons/Badges/badgeHelloGNormal";
 import BadgeHSL from "../assets/icons/Badges/badgeHelloSLocked";
 import BadgeHSN from "../assets/icons/Badges/badgeHelloSNormal";
 
+import BadgeHBNB from "../assets/icons/Badges/badgeHelloBNormalBig";
+import BadgeHSNB from "../assets/icons/Badges/badgeHelloSNormalBig";
+import BadgeHGNB from "../assets/icons/Badges/BadgeHelloGNormalBig";
+import BadgeCBNB from "../assets/icons/Badges/badgeCommBNormalBig";
+import BadgeCSNB from "../assets/icons/Badges/badgeCommSNormalBig";
+import BadgeCGNB from "../assets/icons/Badges/badgeCommGNormalBig";
+import BadgeFBNB from "../assets/icons/Badges/badgeFamilyBNormalBig";
+import BadgeFSNB from "../assets/icons/Badges/badgeFamilySNormalBig";
+import BadgeFGNB from "../assets/icons/Badges/badgeFamilyGNormalBig";
+
+import Facebook from "../assets/icons/facebook";
+import Twitter from "../assets/icons/twitter";
+import AchievementBG from "../assets/icons/achievementBG";
+
 import Empty from "../components/Empty";
 import { LinearGradient } from 'expo-linear-gradient';
+import Share from 'react-native-share';
 
 export default class AchievementScreen extends Component {
 
@@ -46,16 +61,43 @@ export default class AchievementScreen extends Component {
   }
 
   state = {
-    hello: [1, 1, 1],
-    community: [1, 1, 0],
-    family: [0, 0, 0],
+    unlocked: [1, 1, 1, 1, 1, 0, 1, 0, 0],
+    isVisible: [false, false, false, false, false, false, false, false, false],
+    unlockDate: [
+      new Date(), 
+      new Date(1993, 5, 7, 14, 39, 7),
+      new Date(1811, 6, 28, 14, 39, 7),
+      new Date(2018, 6, 28, 14, 39, 7),
+      new Date(2012, 6, 22, 14, 39, 7),
+      NaN,
+      new Date(2002, 3, 8, 14, 39, 7),
+      NaN,
+      NaN,
+    ],
+  };
+
+  toggleModal = (id) => {
+    if (this.state.unlocked[id] === 1) {
+      this.state.isVisible[id] = !this.state.isVisible[id];
+      this.forceUpdate();
+    }
+  };
+
+  sumCount = (i, j, k) => {
+    let unlockedItem = this.state.unlocked;
+    return unlockedItem[i] + unlockedItem[j] + unlockedItem[k];
+  };
+
+  getDateFormat = (id) => {
+    let d = this.state.unlockDate[id];
+    let format = d.toDateString().split(" ");
+    return format[1] + " " + format[2] + ", " + format[3];
   }
 
   render() {
-
-    let helloCompleted = this.state.hello.reduce((a, b) => a + b, 0);
-    let communityCompleted = this.state.community.reduce((a, b) => a + b, 0);
-    let familyCompleted = this.state.family.reduce((a, b) => a + b, 0);
+    let helloCompleted = this.sumCount(0, 1, 2);
+    let communityCompleted = this.sumCount(3, 4, 5);
+    let familyCompleted = this.sumCount(6, 7, 8);
     let completed = helloCompleted + communityCompleted + familyCompleted;
 
     return (
@@ -84,17 +126,19 @@ export default class AchievementScreen extends Component {
             </Text>
           </View>
           <View style={{flexDirection: "row", padding: 10,}}>
-            {(this.state.hello[0] === 0) ? (
-              <BadgeHBL style={styles.badge}></BadgeHBL>
-            ) : (
-              <BadgeHBN style={styles.badge}></BadgeHBN>
-            )}
-            {(this.state.hello[1] === 0) ? (
+            <TouchableOpacity onPress={() => this.toggleModal(0)}>
+              {(this.state.unlocked[0] === 0) ? (
+                <BadgeHBL style={styles.badge}></BadgeHBL>
+              ) : (
+                <BadgeHBN style={styles.badge}></BadgeHBN>
+              )}
+            </TouchableOpacity>
+            {(this.state.unlocked[1] === 0) ? (
               <BadgeHSL style={styles.badge}></BadgeHSL>
             ) : (
               <BadgeHSN style={styles.badge}></BadgeHSN>
             )}
-            {(this.state.hello[2] === 0) ? (
+            {(this.state.unlocked[2] === 0) ? (
               <BadgeHGL style={styles.badge}></BadgeHGL>
             ) : (
               <BadgeHGN style={styles.badge}></BadgeHGN>
@@ -113,17 +157,17 @@ export default class AchievementScreen extends Component {
             </Text>
           </View>
           <View style={{flexDirection: "row", padding: 10,}}>
-            {(this.state.community[0] === 0) ? (
+            {(this.state.unlocked[3] === 0) ? (
               <BadgeCBL style={styles.badge}></BadgeCBL>
             ) : (
               <BadgeCBN style={styles.badge}></BadgeCBN>
             )}
-            {(this.state.community[1] === 0) ? (
+            {(this.state.unlocked[4] === 0) ? (
               <BadgeCSL style={styles.badge}></BadgeCSL>
             ) : (
               <BadgeCSN style={styles.badge}></BadgeCSN>
             )}
-            {(this.state.community[2] === 0) ? (
+            {(this.state.unlocked[5] === 0) ? (
               <BadgeCGL style={styles.badge}></BadgeCGL>
             ) : (
               <BadgeCGN style={styles.badge}></BadgeCGN>
@@ -142,17 +186,17 @@ export default class AchievementScreen extends Component {
             </Text>
           </View>
           <View style={{flexDirection: "row", padding: 10,}}>
-            {(this.state.family[0] === 0) ? (
+            {(this.state.unlocked[6] === 0) ? (
               <BadgeFBL style={styles.badge}></BadgeFBL>
             ) : (
               <BadgeFBN style={styles.badge}></BadgeFBN>
             )}
-            {(this.state.family[1] === 0) ? (
+            {(this.state.unlocked[7] === 0) ? (
               <BadgeFSL style={styles.badge}></BadgeFSL>
             ) : (
               <BadgeFSN style={styles.badge}></BadgeFSN>
             )}
-            {(this.state.family[2] === 0) ? (
+            {(this.state.unlocked[8] === 0) ? (
               <BadgeFGL style={styles.badge}></BadgeFGL>
             ) : (
               <BadgeFGN style={styles.badge}></BadgeFGN>
@@ -170,6 +214,34 @@ export default class AchievementScreen extends Component {
           )}
           {" "}/{" "}09
         </Text>
+
+        <Modal
+          isVisible={this.state.isVisible[0]}
+          onBackdropPress={() => this.toggleModal(0)}
+          animationIn="fadeInUp"
+          animationOut="fadeOutDown"
+          style={styles.modalStyle}
+        >
+          <View style={{flex:1, justifyContent:"center", alignItems:"center", marginLeft: 3}}>
+            <AchievementBG></AchievementBG>
+            <BadgeHBNB style={{marginTop: -75, marginLeft: -3}}></BadgeHBNB>
+          </View>
+          <Empty></Empty>
+          <Empty></Empty>
+          <Empty></Empty>
+          <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+            <Text style={{fontSize: 22, fontWeight: "bold"}}>Hello World</Text>
+            <Text style={{fontSize: 16, marginTop: 15}}>Complete basic tutorial</Text>
+            <Empty></Empty>
+            <Empty></Empty>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>Unlocked on:{" "}{this.getDateFormat(0)}</Text>
+          </View>
+          <View style={styles.share}>
+            <Text style={{fontSize: 12}}>Share on:{" "}</Text>
+            <Facebook style={{marginLeft: 5}}></Facebook>
+            <Twitter style={{marginLeft: 5}}></Twitter>
+          </View>
+        </Modal>
 
       </View>
     );
@@ -217,5 +289,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#fff",
     marginTop: 5,
+  },
+  modalStyle: {
+    borderRadius: 15, 
+    justifyContent: "center",
+    marginVertical: 140,
+    marginHorizontal: 30,
+    backgroundColor: colors.WHITE,
+  },
+  share: {
+    flex:1, 
+    justifyContent:"center", 
+    alignItems:"center", 
+    flexDirection: "row", 
+    marginLeft: 120,
+    marginTop: 10,
   }
 });
