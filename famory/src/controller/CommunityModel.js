fd391f606acfb4a3a1da27fffe6cf03a8f159ee8
sahuang <xiaohaix@student.unimelb.edu.au>
2019-09-15 14:59:25 +1000

    Like works

--- famory/src/controller/CommunityModel.js
+++ famory/src/controller/CommunityModel.js
@@ -1,4 +1,5 @@
 import * as firebase from "firebase";
+import firebaseContainer from "./firebaseConfig";
 
 export class CommunityModelManage{
 
@@ -7,14 +8,15 @@ export class CommunityModelManage{
 
   // get instance of community model manager
   static getInstance(){
-    if(this._managePart == null){
+    firebaseContainer.getInstance().justStart();
+    if (this._managePart == null){
       this._managePart = new CommunityModelManage();
     }
     return this._managePart;
   }
 
-  // cb is the callback when getting the data
-  getCommunity(cb){
+  // get community posts and return as an array
+  getCommunity(callback) {
 
     let communityRef = firebase.database().ref(this._path);
     communityRef.once("value").then((snapshot) => {
@@ -26,10 +28,31 @@ export class CommunityModelManage{
         posts.push(new Post(objectlist[i.toString()]));
       }
 
-      cb(posts);
+      callback(posts);
+    });
+  }
+
+  // update like + 1
+  increaseLike(callback, id) {
+
+    let likeRef = firebase.database().ref(this._path + '/' + id + '/likes');
+    likeRef.once("value").then((snapshot) => {
+      // set like
+      likeRef.set(snapshot.val() + 1);
+    });
+  }
+
+  // update like - 1
+  decreaseLike(callback, id) {
+    
+    let likeRef = firebase.database().ref(this._path + '/' + id + '/likes');
+    likeRef.once("value").then((snapshot) => {
+      // set like
+      likeRef.set(snapshot.val() - 1);
     });
   }
 }
+
 
 // Post class, we can obtain a post object from the class
 export class Post {
@@ -47,15 +70,10 @@ export class Post {
     this.likes = post["likes"];
     this.location = post["location"];
     this.title = post["title"];
-    // for replies, we need an array of json format
-    let id = 0;
-    for (let reply in post["replies"]) {
-      this.replies.push({
-        id: id, 
-        text: reply[id],
-      });
-      id += 1;
-    }
+    // for replies, we need an array of comment
+    this.replies.push(post["replies"]["0"]);
+    this.replies.push(post["replies"]["1"]);
+    this.replies.push(post["replies"]["2"]);
   }
 
   toObject() {
