aff204c1cc787b3adb7d32bca7c921b6d1bac970
Shouyin <7000kl@163.com>
2019-09-16 12:33:21 +1000

    kjnl

--- famory/src/screens/TestScreen.js
+++ famory/src/screens/TestScreen.js
@@ -1,11 +1,12 @@
-import React, {Component} from "react";
-import { View , Alert, Text} from "react-native";
-import Modal from "react-native-modal";
-import { Avatar } from "react-native-elements";
-import ImageButton from "../components/ImageButton";
-import cxk from "../assets/images/logo.png"
-import colors from "../config/colors";
-import Button from "../components/Button";
+import React, { Component } from 'react';
+import {StyleSheet, Image, Alert, View, Text, TextInput, Button} from 'react-native';
+import { Container, Header, Content, ListItem, Icon, Left, Body, Right, Switch, Separator } from 'native-base';
+
+import * as ImagePicker from 'expo-image-picker';
+import { Video } from 'expo-av';
+
+import firebase from "firebase";
+import firebaseContainer from "../controller/firebaseConfig";
 
 export default class TestScreen extends Component{
   static navigationOptions = {
@@ -13,39 +14,120 @@ export default class TestScreen extends Component{
   };
 
   state = {
-    isModalVisible: false
+    image: "https://firebasestorage.googleapis.com/v0/b/fir-one-28de9.appspot.com/o/post-3.jpg?alt=media&token=76e4cd81-d3ba-46ea-9346-890659cf7714",
+    video: null,
+    uploading: false,
   };
 
-  toggleModal = () => {
-    this.setState({ isModalVisible: !this.state.isModalVisible });
+  uploadImage = () => {
+
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+    });
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
   };
-  //sadfafasfas
+
+  _pickVideo = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: true,
+    });
+
+    firebaseContainer.getInstance().justStart();
+    this._handleImagePicked(result);
+  };
+
+  _handleImagePicked = async result => {
+    try {
+      this.setState({ uploading: true });
+
+      if (!result.cancelled) {
+        uploadUrl = await uploadVideoAsync(result.uri);
+        alert(uploadUrl);
+        this.setState({ video: uploadUrl });
+      }
+    } catch (e) {
+      console.log(e);
+      alert('Upload failed, sorry :(');
+    } finally {
+      this.setState({ uploading: false });
+    }
+  };
+
+
   render() {
     return (
       <View style={{ flex: 1, alignItems:"center", justifyContent:"center"}}>
-        <Button label="Show modal" onPress={this.toggleModal} />
-        <Modal isVisible={this.state.isModalVisible} style={{marginVertical:170, backgroundColor:colors.WHITE, borderRadius:15, justifyContent:"center"}}>
-          <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-            <View style={{flex:4, flexDirection:"row", marginVertical:10}}>
-              <View style={{marginHorizontal:10}}>
-                <Avatar
-                icon={{name:"more-horiz", type:"material"}} 
-                rounded
-                size={"medium"}/>
-              </View>
-              
-              <Avatar
-              icon={{name:"more-horiz", type:"material"}} 
-              rounded
-              size={"medium"}/>
-            </View>
-
-            <View style={{flex:1}}>
-              <Button label="Hide modal" onPress={this.toggleModal} extraStyles={{backgroundColor:colors.TORCH_RED, marginVertical:10}}/>
-            </View>
-          </View>
-        </Modal>
+        <View style={{alignItems: "center", }}>
+          <Image source={{uri: this.state.image}}  style={styles.avatar} />
+          <Text style={{fontSize: 30, color: '#347ED3'}} onPress={this._pickImage}>
+            Upload Photo From Local
+          </Text>
+          {(this.state.video === null) ? (
+            <Text style={{fontSize: 30, color: '#347ED3'}} onPress={this._pickVideo}>
+            Upload Video From Local
+          </Text>
+          ) : (
+            <Video
+              source={{ uri: this.state.video }}
+              rate={1.0}
+              volume={1.0}
+              isMuted={false}
+              resizeMode="Video.RESIZE_MODE_CONTAIN"
+              useNativeControls={true}
+              isLooping={true}
+              style={{ width: 350, height: 350 }}
+            />
+          )}
+        </View>
       </View>
     );
   }
 };
+
+async function uploadVideoAsync(uri) {
+
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function(e) {
+      console.log(e);
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+
+  const ref = firebase
+    .storage()
+    .ref()
+    .child('caixukun');
+  const snapshot = await ref.put(blob);
+
+  // We're done with the blob, close and release it
+  blob.close();
+
+  return await snapshot.ref.getDownloadURL();
+}
+
+const styles = StyleSheet.create({
+  avatar: {
+    width: 120,
+    height: 120,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+})
