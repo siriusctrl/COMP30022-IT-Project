112ba2d9a9b373d9968f43fedb1d821b44401f79
sahuang <xiaohaix@student.unimelb.edu.au>
2019-09-21 14:43:28 +1000

    Weird bug

--- famory/src/screens/ArtefactGuideScreen.js
+++ famory/src/screens/ArtefactGuideScreen.js
@@ -1,8 +1,12 @@
-import React, {Component} from "react";
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, ImageBackground } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, TextInput, ImageBackground, Image, TouchableHighlight } from "react-native";
 import colors from "../config/colors";
 import { Button, Icon, ListItem, Body } from 'native-base';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
+
+import firebase from "firebase";
+import firebaseContainer from "../controller/firebaseConfig";
+import * as ImagePicker from 'expo-image-picker';
 
 export default class ArtefactGuide extends Component{
 
@@ -14,11 +18,12 @@ export default class ArtefactGuide extends Component{
     selected: 0,
     name: "",
     description: "",
-    itemUri: null,
+    item: "https://firebasestorage.googleapis.com/v0/b/fir-one-28de9.appspot.com/o/post-3.jpg?alt=media&token=76e4cd81-d3ba-46ea-9346-890659cf7714",
+    uploaded: false,
     textArtefact: "",
     currentStage: "addArtefactFromNewInitial",
     currentPurpose: "addNewArtefact",
-  }
+  };
 
   initialStage = {
     addNewArtefact: "addArtefactFromNewInitial"
@@ -208,8 +213,8 @@ export default class ArtefactGuide extends Component{
               <Icon name='arrow-back' />
               <Text style={guideStyle.bottomButtonLeft}>Back</Text>
             </Button>
-            <Button iconRight light onPress={() => this._changeStage(false)}>
-              <Text style={guideStyle.bottomButtonRight}>Next</Text>
+            <Button iconRight light onPress={() => alert("Finished!")}>
+              <Text style={guideStyle.finishButton}>DONE</Text>
               <Icon name='arrow-forward' style={{marginRight: 15}} />
             </Button>
             
@@ -228,13 +233,25 @@ export default class ArtefactGuide extends Component{
       "title": "Image Upload",
       "view": () =>
         <View style={{flex: 4, flexDirection: "column", paddingTop: 10}}>
+          {(this.state.uploaded) ? (
+            <TouchableOpacity style={guideStyle.uploadBox} onPress={this._upload} >
+              <Image source={{uri: this.state.item}}></Image>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity style={guideStyle.uploadBox} onPress={this._upload} >
+              <Ionicons name="md-images" size={44} color="orange" />
+              <Text style={guideStyle.uploadText}>Browse</Text>
+            </TouchableOpacity>
+          )}
+
+          <Image source={{uri: this.state.item}}></Image>
 
           <View style={guideStyle.bottomButtonCn}>
-            <Button iconLeft light onPress={() => this._changeStage(true)}>
+            <Button iconLeft light onPress={() => this._changeStage(true)} >
               <Icon name='arrow-back' />
               <Text style={guideStyle.bottomButtonLeft}>Back</Text>
             </Button>
-            <Button iconRight light onPress={() => this._changeStage(false)}>
+            <Button iconRight light onPress={() => alert("Finished!")} style={{opacity: 0}}>
               <Text style={guideStyle.bottomButtonRight}>Next</Text>
               <Icon name='arrow-forward' style={{marginRight: 15}} />
             </Button>
@@ -263,9 +280,9 @@ export default class ArtefactGuide extends Component{
       if (this.state.selected == 0) {
         now = "textnext";
       } else if (this.state.selected == 1) {
-        now = "videonext";
-      } else {
         now = "photonext";
+      } else {
+        now = "videonext";
       }
     }
 
@@ -298,13 +315,35 @@ export default class ArtefactGuide extends Component{
         ... te
       }
     );
+  }
 
+  // upload image or video from local system
+  _upload = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+    });
+    if (!result.cancelled) {
+      this.state.item = result.uri;
+      this.state.uploaded = true;
+      this.forceUpdate();
+    };
+    firebaseContainer.getInstance().justStart();
+    alert(this.state.item);
+    if (!result.cancelled) {
+      // upload item to firebase
+      uploadUrl = await _uploadToFirebase(result.uri);
+      return uploadUrl;
+    }
   }
 
   render() {
     return(
       <View style={{flexDirection: "column", flex: 1}}>
-
+        <Image source={{uri: this.state.item}}></Image>
+        <Image source={{uri: this.state.item}}></Image>
+        <Image source={{uri: this.state.item}}></Image>
         <View style={{paddingTop: 26, paddingHorizontal: 26, flex: 1, justifyContent: "flex-start", alignItems: "center", flexDirection: "row"}}>
           <Icon name='close' />
         </View>
@@ -339,6 +378,14 @@ const guideStyle = StyleSheet.create({
     color: colors.DODGER_BLUE, 
     fontSize: 16
   },
+  finishButton: {
+    height: 58, 
+    width: 68, 
+    textAlign: "center", 
+    textAlignVertical: "center", 
+    color: 'green', 
+    fontSize: 16
+  },
   bottomButtonCn: {
     paddingHorizontal: 20, 
     paddingBottom: 26, 
@@ -370,6 +417,13 @@ const guideStyle = StyleSheet.create({
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 10,
     marginLeft: 50,
+  },
+  uploadText: {
+    fontSize: 26, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+    color: colors.SILVER,
+    marginTop: 5,
   },
   unselectedText: {
     fontSize: 26, 
@@ -424,5 +478,52 @@ const guideStyle = StyleSheet.create({
     height: 340,
     letterSpacing: 2,
     fontFamily: 'almond',
-  }
+  }, 
+  uploadBox: {
+    flex: 1,
+    marginLeft: 20,
+    marginRight: 20,
+    height: 220,
+    borderStyle: 'dashed',
+    borderRadius: 1,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+  avatar: {
+    resizeMode: "contain",
+    alignSelf: "center",
+  },
 });
+
+_uploadToFirebase = async uri => {
+
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function(e) {
+      console.log(e);
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+
+  // randomly generate a suffix for artefact using crypto
+  var rdmString = "";
+  for( ;rdmString.length < 11; rdmString  += Math.random().toString(36).substr(2));
+  const ref = firebase
+    .storage()
+    .ref()
+    .child('artefact_' + rdmString.substr(0, 11));
+  const snapshot = await ref.put(blob);
+
+  // We're done with the blob, close and release it
+  blob.close();
+
+  return await snapshot.ref.getDownloadURL();
+}
