0d2f6420de48d97a9d76530e483e35e636731371
Miley <mileyzha@gmail.com>
2019-10-01 17:00:03 +1000

    Share with image in camera roll

--- famory/src/config/strings.js
+++ famory/src/config/strings.js
@@ -15,6 +15,13 @@ const strings = {
     WECHAT: "weixin://",
     GMAIL: "mailto://",
     INSTAGRAM: "https://www.instagram.com/create/story",
+    GOLDTROPHY: "https://firebasestorage.googleapis.com/v0/b/fir-one-28de9.appspot.com" +
+      "/o/gold_trophy.png?alt=media&token=e4f85a56-2896-4ddc-953e-342bc9e271e6",
+    SILVERTROPHY: "https://firebasestorage.googleapis.com/v0/b/fir-one-28de9.appspot.com" +
+      "/o/silver-trophy.png?alt=media&token=3d6f8202-6b10-411f-af00-451a3ddb015c",
+    BRONZETROPHY: "https://firebasestorage.googleapis.com/v0/b/fir-one-28de9.appspot.com" +
+      "/o/bronze-trophy.png?alt=media&token=e69b1756-29cd-4016-bd10-5924ec51c57c",
+    DOWNLOADLINK: "https://tiny.cc/de5fdz",
   };
   
   export default strings;
