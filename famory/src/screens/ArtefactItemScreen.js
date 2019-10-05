f2e71480e02726d1a820d8d88b70fd25cf299b68
sahuang <xiaohaix@student.unimelb.edu.au>
2019-10-05 14:52:18 +1000

    3d model

--- famory/src/screens/ArtefactItemScreen.js
+++ famory/src/screens/ArtefactItemScreen.js
@@ -6,13 +6,13 @@ import * as WebBrowser from "expo-web-browser";
 import { Icon } from 'native-base';
 import Modal from "react-native-modal";
 
-import { Entypo, AntDesign, FontAwesome } from '@expo/vector-icons';
+import { Entypo, AntDesign, FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import Wechat from "../assets/icons/wechat";
 import Messenger from "../assets/icons/messenger";
 import GMail from "../assets/icons/gmail";
 import Instagram from "../assets/icons/instagram";
 
-
+import { WebView } from 'react-native-webview';
 import { Video } from 'expo-av';
 import ArtCard from "../components/ArtCard";
 import { TouchableNativeFeedback } from "react-native-gesture-handler";
@@ -32,10 +32,10 @@ const optionDropdown = {
 let options = [
   'Cancel', 
   <View style={optionDropdown}>
-    <FontAwesome name='street-view' size={20}/> 
+    <MaterialIcons name='3d-rotation' size={20}/> 
     <View style={{flexDirection: 'column', flex: 1, marginLeft: 30}}>
-      <Text style={{fontSize: 16, fontWeight: '400'}}>AR View</Text>
-      <Text style={{fontSize: 12, fontWeight: 'normal'}}>Experience Augmented Reality (In experiment)</Text>
+      <Text style={{fontSize: 16, fontWeight: '400'}}>Visualisation</Text>
+      <Text style={{fontSize: 12, fontWeight: 'normal'}}>Experience 3D Model (In experiment)</Text>
     </View>
   </View>, 
   <View style={optionDropdown}>
@@ -62,8 +62,8 @@ export default class ArtefactItem extends Component{
     artefactItem: this.props.navigation.getParam("item", null),
     videoFinishedLoading: false,
     width: 300,
-    height: 350
-
+    height: 350,
+    arVisible: false,
   }
 
   // display action sheet
@@ -74,6 +74,12 @@ export default class ArtefactItem extends Component{
   // update the modal for sharing
   toggleModal = () => {
     this.state.modalVisible = !this.state.modalVisible;
+    this.forceUpdate();
+  };
+
+  // update the modal for AR web view
+  toggleAR = () => {
+    this.state.arVisible = !this.state.arVisible;
     this.forceUpdate();
   };
 
@@ -114,87 +120,82 @@ export default class ArtefactItem extends Component{
           </TouchableNativeFeedback>
         </View>
         <View style={{flex: 7, overflow: "visible"}}>
-        <ScrollView style={{flex: 1, overflow: "visible"}}>
-        {this.state.artefactItem.type == "video"? 
-        <View style={{marginHorizontal: 29, marginVertical: 19, marginBottom: 68, flexDirection: "column", minHeight: 870, overflow: "visible"}}>
-          <Video
-              source={{ uri: this.state.artefactItem.content }}
-              rate={1.0}
-              volume={1.0}
-              isMuted={false}
-              resizeMode="cover"
-              useNativeControls={true}
-              isLooping={false}
+          <ScrollView style={{flex: 1, overflow: "visible"}}>
+            {this.state.artefactItem.type == "video"? 
+            <View style={{marginHorizontal: 29, marginVertical: 19, marginBottom: 68, flexDirection: "column", minHeight: 870, overflow: "visible"}}>
+              <Video
+                  source={{ uri: this.state.artefactItem.content }}
+                  rate={1.0}
+                  volume={1.0}
+                  isMuted={false}
+                  resizeMode="cover"
+                  useNativeControls={true}
+                  isLooping={false}
 
-              onReadyForDisplay={(vid) => {
-                this.setState({
-                  height: this.state.width * vid.naturalSize.height / vid.naturalSize.width
-                })
-              }}
-              
-
-              style={{width: this.state.width, height: this.state.height}}
-
-            />
-            <Text style={{color: colors.AGRAY, marginTop: 36, fontStyle: "italic"}}>{this.state.artefactItem.description}</Text>
-            <View style={{marginTop: 58}}>
-              <Text style={{color: colors.AGRAY}}>DETAIL</Text>
-              <View
-              style={{
-                borderBottomColor: colors.AGRAY,
-                borderBottomWidth: 0.5,
-              }}
-            />
-              <View style={{marginTop: 17}}>
-                <Text style={{color: colors.AGRAY}}>CREATE DATE</Text>
-                <Text style={{color: colors.AGRAY}}>{this.state.artefactItem.dateAdded}</Text>
+                  onReadyForDisplay={(vid) => {
+                    this.setState({
+                      height: this.state.width * vid.naturalSize.height / vid.naturalSize.width
+                    })
+                  }}
+                  style={{width: this.state.width, height: this.state.height}}
+                />
+                <Text style={{color: colors.AGRAY, marginTop: 36, fontStyle: "italic"}}>{this.state.artefactItem.description}</Text>
+                <View style={{marginTop: 58}}>
+                  <Text style={{color: colors.AGRAY}}>DETAIL</Text>
+                  <View
+                  style={{
+                    borderBottomColor: colors.AGRAY,
+                    borderBottomWidth: 0.5,
+                  }}
+                />
+                  <View style={{marginTop: 17}}>
+                    <Text style={{color: colors.AGRAY}}>CREATE DATE</Text>
+                    <Text style={{color: colors.AGRAY}}>{this.state.artefactItem.dateAdded}</Text>
+                  </View>
+                </View>
+              </View>: this.state.artefactItem.type == "text"? 
+              <View style={{marginHorizontal: 29, marginVertical: 19, flexDirection: "column", minHeight: 870, overflow: "visible"}}>
+                <Text style={{lineHeight: 32, fontSize: 23, color: "#6c6c6c"}}>
+                  {this.state.artefactItem.name}
+                </Text>
+                <Text style={{lineHeight: 32, fontSize: 18, color: "#6c6c6c", marginTop: 32}}>
+                  {this.state.artefactItem.content}
+                </Text>
+                <Text style={{color: colors.AGRAY, marginTop: 36, fontStyle: "italic"}}>{this.state.artefactItem.description}</Text>
+                <View style={{marginTop: 58}}>
+                  <Text style={{color: colors.AGRAY}}>DETAIL</Text>
+                  <View
+                  style={{
+                    borderBottomColor: colors.AGRAY,
+                    borderBottomWidth: 0.5,
+                  }}
+                />
+                  <View style={{marginTop: 17}}>
+                    <Text style={{color: colors.AGRAY}}>CREATE DATE</Text>
+                    <Text style={{color: colors.AGRAY}}>{this.state.artefactItem.dateAdded}</Text>
+                  </View>
+                </View>
               </View>
-            </View>
-          </View>: this.state.artefactItem.type == "text"? 
-          <View style={{marginHorizontal: 29, marginVertical: 19, flexDirection: "column", minHeight: 870, overflow: "visible"}}>
-            <Text style={{lineHeight: 32, fontSize: 23, color: "#6c6c6c"}}>
-              {this.state.artefactItem.name}
-            </Text>
-            <Text style={{lineHeight: 32, fontSize: 18, color: "#6c6c6c", marginTop: 32}}>
-              {this.state.artefactItem.content}
-            </Text>
-            <Text style={{color: colors.AGRAY, marginTop: 36, fontStyle: "italic"}}>{this.state.artefactItem.description}</Text>
-            <View style={{marginTop: 58}}>
-              <Text style={{color: colors.AGRAY}}>DETAIL</Text>
-              <View
-              style={{
-                borderBottomColor: colors.AGRAY,
-                borderBottomWidth: 0.5,
-              }}
-            />
-              <View style={{marginTop: 17}}>
-                <Text style={{color: colors.AGRAY}}>CREATE DATE</Text>
-                <Text style={{color: colors.AGRAY}}>{this.state.artefactItem.dateAdded}</Text>
+              :
+              <View style={{marginHorizontal: 29, marginVertical: 19, flexDirection: "column", minHeight: 870, overflow: "visible"}}>
+                <ArtCard item={this.state.artefactItem} style={styles.artCard}/>
+                <Text style={{color: colors.AGRAY, marginTop: 36, fontStyle: "italic"}}>{this.state.artefactItem.description}</Text>
+                <View style={{marginTop: 58}}>
+                  <Text style={{color: colors.AGRAY}}>DETAIL</Text>
+                  <View
+                  style={{
+                    borderBottomColor: colors.AGRAY,
+                    borderBottomWidth: 0.5,
+                  }}
+                />
+                  <View style={{marginTop: 17}}>
+                    <Text style={{color: colors.AGRAY}}>CREATE DATE</Text>
+                    <Text style={{color: colors.AGRAY}}>{this.state.artefactItem.dateAdded}</Text>
+                  </View>
+                </View>
               </View>
-            </View>
-          </View>
-          :
-          <View style={{marginHorizontal: 29, marginVertical: 19, flexDirection: "column", minHeight: 870, overflow: "visible"}}>
-            <ArtCard item={this.state.artefactItem} style={styles.artCard}/>
-            <Text style={{color: colors.AGRAY, marginTop: 36, fontStyle: "italic"}}>{this.state.artefactItem.description}</Text>
-            <View style={{marginTop: 58}}>
-              <Text style={{color: colors.AGRAY}}>DETAIL</Text>
-              <View
-              style={{
-                borderBottomColor: colors.AGRAY,
-                borderBottomWidth: 0.5,
-              }}
-            />
-              <View style={{marginTop: 17}}>
-                <Text style={{color: colors.AGRAY}}>CREATE DATE</Text>
-                <Text style={{color: colors.AGRAY}}>{this.state.artefactItem.dateAdded}</Text>
-              </View>
-            </View>
-          </View>
-        
-        }
-          
-        </ScrollView>
+            }  
+          </ScrollView>
         </View>
 
         <Modal
@@ -228,6 +229,18 @@ export default class ArtefactItem extends Component{
           </View>
         </Modal>
 
+        <Modal
+          isVisible={this.state.arVisible}
+          onBackdropPress={() => this.toggleAR()}
+          animationIn="slideInUp"
+          animationOut="fadeOutDown"
+          style={styles.arStyle}
+        >
+          <View style={{flex:1, justifyContent:"center", alignItems:"center", flexDirection:"row"}}>
+            <WebView source={{ uri: strings.ARLINK }} />
+          </View>
+        </Modal>
+
         <ActionSheet
           ref={o => this.ActionSheet = o}
           options={options}
@@ -237,7 +250,8 @@ export default class ArtefactItem extends Component{
           onPress={(index) => { 
             switch (index) {
               case 1:
-                alert("Feature still in development ^_^");
+                // AR
+                this.toggleAR();
                 break;
               case 2:
                 // share on facebook or twitter
@@ -298,5 +312,12 @@ const styles = StyleSheet.create({
     width: 365,
     marginLeft: 0,
     backgroundColor: "#F8F8FF",
+  },
+  arStyle: {
+    borderRadius: 15,
+    justifyContent: "center",
+    marginVertical: 100,
+    marginHorizontal: 30,
+    backgroundColor: 'transparent',
   },
 });
