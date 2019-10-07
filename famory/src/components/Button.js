e4a8cbb71d83ce659a88db28fac99a96d5c8193b
Shouyin <7000kl@163.com>
2019-10-07 14:00:29 +1100

    mix account model and account hold page fixed

--- famory/src/components/Button.js
+++ famory/src/components/Button.js
@@ -19,15 +19,15 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: colors.LIGHTBLUE,
-        marginTop: 12,
-        paddingVertical: 12,
-        borderRadius: 10,
+        height: 54,
+        borderRadius: 27,
         elevation: 8,
         width: "80%",
     },
     text: {
         color: colors.WHITE,
         textAlign: "center",
+        fontSize: 16,
         height: 20,
     }
 });
